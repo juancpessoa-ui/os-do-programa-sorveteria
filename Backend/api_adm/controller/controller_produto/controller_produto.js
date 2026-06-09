5a8c97fd335cfca40810196da0b328cdf4b60df5
julioau15 <julioaugustov@gmail.com>
2026-06-09 17:28:11 -0300

    adiciona codigo para manipulação de dados das tabelas intermediarias na controller produto da api adm

--- Backend/api_adm/controller/controller_produto/controller_produto.js
+++ Backend/api_adm/controller/controller_produto/controller_produto.js
@@ -8,6 +8,14 @@
 const config_message = require('../module/configMessages.js')
 const produtoDAO = require('../../model/DAO/produto/produto.js')
 
+// import das controllers
+const controllerProdutoCategoria = require('./controller_produto_categoria.js')
+const controllerProdutoIngrediente = require('./controller_produto_ingrediente.js')
+const controllerProdutoLote = require('./controller_produto_lote.js')
+const controllerProdutoPromocao = require('./controller_produto_promocao.js')
+const controllerProdutoSabor = require('./controller_produto_sabor.js')
+const controllerProdutoTag = require('./controller_produto_tag.js')
+
 // inserir nova produto
 const inserirNovoProduto = async (produto, contentType) => {
     let message = JSON.parse(JSON.stringify(config_message))
@@ -18,8 +26,62 @@ const inserirNovoProduto = async (produto, contentType) => {
         let result = await produtoDAO.insertProduto(produto)
 
         if(!result) return message.ERROR_INTERNAL_SERVER_MODEL
-
         produto.id = result
+
+        // Manipulação de dados para inserir as categorias no produto
+        for(categoria of produto.categoria){
+            let produtoCategoria = { "id_produto": produto.id, "id_categoria": categoria.id}
+
+            let resultInsertCategoria = await controllerProdutoCategoria.inserirNovoProdutoCategoria(produtoCategoria, contentType)
+
+            if(!resultInsertCategoria.status)  return message.SUCESS_CREATED_ITEM_WARNING // 201
+        }
+
+        // Manipulação de dados para inserir os ingredientes no produto
+        for(ingrediente of produto.ingrediente){
+            let produtoIngrediente = { "id_produto": produto.id, "id_ingrediente": ingrediente.id}
+
+            let resultInsertIngrediente = await controllerProdutoIngrediente.inserirNovoProdutoIngrediente(produtoIngrediente, contentType)
+
+            if(!resultInsertIngrediente.status)  return message.SUCESS_CREATED_ITEM_WARNING // 201
+        }
+
+        // Manipulação de dados para inserir os lotes no produto
+        for(lote of produto.lote){
+            let produtoLote = { "id_produto": produto.id, "id_lote": lote.id}
+
+            let resultInsertLote = await controllerProdutoLote.inserirNovoProdutoLote(produtoLote, contentType)
+
+            if(!resultInsertLote.status)  return message.SUCESS_CREATED_ITEM_WARNING // 201
+        }
+
+        // Manipulação de dados para inserir as promoções no produto
+        for(promocao of produto.promocao){
+            let produtoPromocao = { "id_produto": produto.id, "id_promocao": promocao.id}
+
+            let resultInsertPromocao = await controllerProdutoPromocao.inserirNovoProdutoPromocao(produtoPromocao, contentType)
+
+            if(!resultInsertPromocao.status)  return message.SUCESS_CREATED_ITEM_WARNING // 201
+        }
+
+        // Manipulação de dados para inserir os sabores no produto
+        for(sabor of produto.sabor){
+            let produtoSabor = { "id_produto": produto.id, "id_sabor": sabor.id}
+
+            let resultInsertSabor = await controllerProdutoSabor.inserirNovoProdutoSabor(produtoSabor, contentType)
+
+            if(!resultInsertSabor.status)  return message.SUCESS_CREATED_ITEM_WARNING // 201
+        }
+
+        // Manipulação de dados para inserir as tags no produto
+        for(tag of produto.tag){
+            let produtoTag = { "id_produto": produto.id, "id_tag": tag.id}
+
+            let resultInsertTag = await controllerProdutoTag.inserirNovoProdutoTag(produtoTag, contentType)
+
+            if(!resultInsertTag.status)  return message.SUCESS_CREATED_ITEM_WARNING // 201
+        }
+
         return await montarMensagem(message, message.SUCESS_CREATED_ITEM, produto)
 
     } catch (error) {console.log(error)}
@@ -42,6 +104,84 @@ const atualizarProduto = async (produto, id, contentType) => {
 
         if(!result) return message.ERROR_INTERNAL_SERVER_MODEL // 500
 
+        // Exclui e recria categorias
+        let resultDeleteCategoria = await controllerProdutoCategoria.excluirCategoriasIdProduto(produto.id)
+
+        if(resultDeleteCategoria.status){
+            for(categoria of produto.categoria){
+                let produtoCategoria = { "id_produto": produto.id, "id_categoria": categoria.id}
+
+                let resultInsertCategoria = await controllerProdutoCategoria.inserirNovoProdutoCategoria(produtoCategoria, contentType)
+
+                if(!resultInsertCategoria.status)  return message.SUCESS_CREATED_ITEM_WARNING
+            }
+        }
+
+        // Exclui e recria ingredientes
+        let resultDeleteIngrediente = await controllerProdutoIngrediente.excluirIngredientesIdProduto(produto.id)
+
+        if(resultDeleteIngrediente.status){
+            for(ingrediente of produto.ingrediente){
+                let produtoIngrediente = { "id_produto": produto.id, "id_ingrediente": ingrediente.id}
+
+                let resultInsertIngrediente = await controllerProdutoIngrediente.inserirNovoProdutoIngrediente(produtoIngrediente, contentType)
+
+                if(!resultInsertIngrediente.status)  return message.SUCESS_CREATED_ITEM_WARNING
+            }
+        }
+
+        // Exclui e recria lotes
+        let resultDeleteLote = await controllerProdutoLote.excluirLotesIdProduto(produto.id)
+
+        if(resultDeleteLote.status){
+            for(lote of produto.lote){
+                let produtoLote = { "id_produto": produto.id, "id_lote": lote.id}
+
+                let resultInsertLote = await controllerProdutoLote.inserirNovoProdutoLote(produtoLote, contentType)
+
+                if(!resultInsertLote.status)  return message.SUCESS_CREATED_ITEM_WARNING
+            }
+        }
+
+        // Exclui e recria promoções
+        let resultDeletePromocao = await controllerProdutoPromocao.excluirPromocoesIdProduto(produto.id)
+
+        if(resultDeletePromocao.status){
+            for(promocao of produto.promocao){
+                let produtoPromocao = { "id_produto": produto.id, "id_promocao": promocao.id}
+
+                let resultInsertPromocao = await controllerProdutoPromocao.inserirNovoProdutoPromocao(produtoPromocao, contentType)
+
+                if(!resultInsertPromocao.status)  return message.SUCESS_CREATED_ITEM_WARNING
+            }
+        }
+
+        // Exclui e recria sabores
+        let resultDeleteSabor = await controllerProdutoSabor.excluirSaboresIdProduto(produto.id)
+
+        if(resultDeleteSabor.status){
+            for(sabor of produto.sabor){
+                let produtoSabor = { "id_produto": produto.id, "id_sabor": sabor.id}
+
+                let resultInsertSabor = await controllerProdutoSabor.inserirNovoProdutoSabor(produtoSabor, contentType)
+
+                if(!resultInsertSabor.status)  return message.SUCESS_CREATED_ITEM_WARNING
+            }
+        }
+
+        // Exclui e recria tags
+        let resultDeleteTag = await controllerProdutoTag.excluirTagsIdProduto(produto.id)
+
+        if(resultDeleteTag.status){
+            for(tag of produto.tag){
+                let produtoTag = { "id_produto": produto.id, "id_tag": tag.id}
+
+                let resultInsertTag = await controllerProdutoTag.inserirNovoProdutoTag(produtoTag, contentType)
+
+                if(!resultInsertTag.status)  return message.SUCESS_CREATED_ITEM_WARNING
+            }
+        }
+
         return await montarMensagem(message, message.SUCESS_UPDATE_ITEM, produto)
 
     } catch (error) {console.log(error)}
@@ -59,6 +199,38 @@ const listarProduto = async () => {
 
         // verfica se o array é vazio
         if(result.length <= 0) return message.ERROR_NOT_FOUND // status_code 404
+
+        for(produto of result){
+            let categoria = await controllerProdutoCategoria.buscarCategoriasIdProduto(produto.id)
+            if(categoria.status){
+                produto.categoria = categoria.response.produtoCategoria
+            }
+
+            let ingrediente = await controllerProdutoIngrediente.buscarIngredientesIdProduto(produto.id)
+            if(ingrediente.status){
+                produto.ingrediente = ingrediente.response.produtoIngrediente
+            }
+
+            let lote = await controllerProdutoLote.buscarLotesIdProduto(produto.id)
+            if(lote.status){
+                produto.lote = lote.response.produtoLote
+            }
+
+            let promocao = await controllerProdutoPromocao.buscarPromocoesIdProduto(produto.id)
+            if(promocao.status){
+                produto.promocao = promocao.response.produtoPromocao
+            }
+
+            let sabor = await controllerProdutoSabor.buscarSaboresIdProduto(produto.id)
+            if(sabor.status){
+                produto.sabor = sabor.response.produtoSabor
+            }
+
+            let tag = await controllerProdutoTag.buscarTagsIdProduto(produto.id)
+            if(tag.status){
+                produto.tag = tag.response.produtoTag
+            }
+        }
 
         let listarProdutoMessage = await montarMensagem(message, message.SUCESS_RESPONSE, result)
         message.DEFAULT_MESSAGE.response.count = result.length
@@ -83,6 +255,38 @@ const buscarProduto = async (id) => {
         if(!result) return message.ERROR_INTERNAL_SERVER_MODEL // 500
 
         if(result.length < 1) return config_message.ERROR_NOT_FOUND
+
+        for(produto of result){
+            let categoria = await controllerProdutoCategoria.buscarCategoriasIdProduto(produto.id)
+            if(categoria.status){
+                produto.categoria = categoria.response.produtoCategoria
+            }
+
+            let ingrediente = await controllerProdutoIngrediente.buscarIngredientesIdProduto(produto.id)
+            if(ingrediente.status){
+                produto.ingrediente = ingrediente.response.produtoIngrediente
+            }
+
+            let lote = await controllerProdutoLote.buscarLotesIdProduto(produto.id)
+            if(lote.status){
+                produto.lote = lote.response.produtoLote
+            }
+
+            let promocao = await controllerProdutoPromocao.buscarPromocoesIdProduto(produto.id)
+            if(promocao.status){
+                produto.promocao = promocao.response.produtoPromocao
+            }
+
+            let sabor = await controllerProdutoSabor.buscarSaboresIdProduto(produto.id)
+            if(sabor.status){
+                produto.sabor = sabor.response.produtoSabor
+            }
+
+            let tag = await controllerProdutoTag.buscarTagsIdProduto(produto.id)
+            if(tag.status){
+                produto.tag = tag.response.produtoTag
+            }
+        }
 
         return await montarMensagem(message, message.SUCESS_RESPONSE, result)
 
@@ -120,17 +324,17 @@ const validarDados = async (produto, contentType) => {
         return message.ERROR_BAD_REQUEST // 400
     }
 
-    if(produto.descricao == undefined || produto.descricao == null || produto.descricao == '' || produto.descricao.length > 5 || typeof(produto.descricao) != 'number'){
-        message.ERROR_BAD_REQUEST.field = '[DESCRIÇÃO] INVÁLIDO'
+    if(produto.descricao == undefined || produto.descricao == null || produto.descricao == '' || produto.descricao.length > 255 || typeof(produto.descricao) != 'string'){
+        message.ERROR_BAD_REQUEST.field = '[DESCRIÇÃO] INVÁLIDA'
         return message.ERROR_BAD_REQUEST // 400
     }
 
-    if(produto.preco == undefined || produto.preco == null || produto.preco == '' || Number(produto.preco).toFixed(2).length > 6 || typeof(produto.preco) != 'number'){
+    if(produto.preco == undefined || produto.preco == null || produto.preco == '' || isNaN(produto.preco) || Number(produto.preco).toFixed(2).length > 6){
         message.ERROR_BAD_REQUEST.field = '[PREÇO] INVÁLIDO'
         return message.ERROR_BAD_REQUEST // 400
     }
 
-    if(produto.status == undefined || produto.status == null || produto.status == '' || String(produto.status).length > 1 || typeof(produto.status) != 'number'){
+    if(produto.status == undefined || produto.status == null || produto.status == '' || isNaN(produto.status) || (Number(produto.status) != 0 && Number(produto.status) != 1)){
         message.ERROR_BAD_REQUEST.field = '[STATUS] INVÁLIDO'
         return message.ERROR_BAD_REQUEST // 400
     }
@@ -141,7 +345,37 @@ const validarDados = async (produto, contentType) => {
     }
 
     if(produto.img == undefined || produto.img == null || produto.img == '' || produto.img.length > 255 || typeof(produto.img) != 'string'){
-        message.ERROR_BAD_REQUEST.field = '[IMG] INVÁLIDO'
+        message.ERROR_BAD_REQUEST.field = '[IMG] INVÁLIDA'
+        return message.ERROR_BAD_REQUEST // 400
+    }
+
+    if(!Array.isArray(produto.categoria)){
+        message.ERROR_BAD_REQUEST.field = '[CATEGORIA] INVÁLIDA'
+        return message.ERROR_BAD_REQUEST // 400
+    }
+
+    if(!Array.isArray(produto.ingrediente)){
+        message.ERROR_BAD_REQUEST.field = '[INGREDIENTE] INVÁLIDO'
+        return message.ERROR_BAD_REQUEST // 400
+    }
+
+    if(!Array.isArray(produto.lote)){
+        message.ERROR_BAD_REQUEST.field = '[LOTE] INVÁLIDO'
+        return message.ERROR_BAD_REQUEST // 400
+    }
+
+    if(!Array.isArray(produto.promocao)){
+        message.ERROR_BAD_REQUEST.field = '[PROMOÇÃO] INVÁLIDA'
+        return message.ERROR_BAD_REQUEST // 400
+    }
+
+    if(!Array.isArray(produto.sabor)){
+        message.ERROR_BAD_REQUEST.field = '[SABOR] INVÁLIDO'
+        return message.ERROR_BAD_REQUEST // 400
+    }
+
+    if(!Array.isArray(produto.tag)){
+        message.ERROR_BAD_REQUEST.field = '[TAG] INVÁLIDA'
         return message.ERROR_BAD_REQUEST // 400
     }
 
@@ -168,7 +402,6 @@ const montarMensagem = async (base,status,response = null) => {
 
     return base.DEFAULT_MESSAGE // 200 ou 201
 }
-
 
 module.exports = {
     inserirNovoProduto,
